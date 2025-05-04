7350f659d591a27a6f98a2c3997a0a60b0f117af
Ecy Arvy <128708785+esyaar@users.noreply.github.com>
2025-05-04 12:57:22 +0700

    1

---
+++ ManajemenLTT/app/login.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import {StyleSheet, TextInput} from 'react-native';
+import {SafeAreaView, SafeAreaProvider} from 'react-native-safe-area-context';
+
+export default function login(){
+    return(
+        <SafeAreaView></SafeAreaView>
+    )
+}
